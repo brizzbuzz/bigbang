4f2b859b79ded8207b56f06c3500e7eb24e241aa
Ryan Brink <dev@ryanbr.ink>
2024-01-07 17:34:47 -0500

    chore: lauch 1password on startup in silent mode

--- dots/hypr/start.sh
+++ dots/hypr/start.sh
@@ -6,5 +6,8 @@ swww init &
 # Set Wallpaper
 swww img ~/Pictures/pastel-valley.png &
 
-# Init
+# Init Waybar
 waybar &
+
+# Load 1Password in silent mode to activate SSH socket
+1password --silent &
