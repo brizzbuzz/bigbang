31666e971b9e11af587f5c3f5144ba99600802ae
Ryan Brink <dev@ryanbr.ink>
2024-01-12 11:37:20 -0500

    chore: formatting

--- home/dev.nix
+++ home/dev.nix
@@ -4,15 +4,15 @@
   ...
 }: {
   home.packages = with pkgs; [
-    cargo           # Rust package manager
-    gcc9            # C compiler
-    graphviz-nox    # GraphViz / Dot
-    helmfile        # Declarative helm release management
-    jdk21           # Java 21 JDK
+    cargo # Rust package manager
+    gcc9 # C compiler
+    graphviz-nox # GraphViz / Dot
+    helmfile # Declarative helm release management
+    jdk21 # Java 21 JDK
     kubernetes-helm # Kubernetes "package manager"
-    lldb_9          # Debugger Protocol
-    lua             # Lua
-    rust-analyzer   # LSP for Rust
-    zig             # Zig stdlib
+    lldb_9 # Debugger Protocol
+    lua # Lua
+    rust-analyzer # LSP for Rust
+    zig # Zig stdlib
   ];
 }
--- home/terminal.nix
+++ home/terminal.nix
@@ -5,29 +5,29 @@
 }: {
   home = {
     packages = with pkgs; [
-      alacritty     # Terminal Emulator 
-      atuin         # Magical Shell History
-      bat           # Sexy cat
-      bottom        # TUI System Monitoring
-      difftastic    # Sytnax aware diff tool
-      du-dust       # TUI folder size tool
-      dua           # TUI disk space tool 
-      fastfetch     # Neofetch but faster
-      fd            # Sexy find
-      fzf           # Fuzzy search tool
-      gh            # GitHub CLI
-      gitui         # TUI for git
-      glow          # CLI markdown renderer
-      kubectl       # Official kubernetes CLI
-      k9s           # Kubernetes cluster TUI
-      lazygit       # Another TUI for git
-      ripgrep       # Text search
-      sd            # Sexy sed
-      starship      # Prompt configuration
-      xh            # CLI Http Client
-      xplr          # TUI File Explorer
-      zellij        # Terminal Multiplexer
-      zoxide        # Directory Portal 
+      alacritty # Terminal Emulator
+      atuin # Magical Shell History
+      bat # Sexy cat
+      bottom # TUI System Monitoring
+      difftastic # Sytnax aware diff tool
+      du-dust # TUI folder size tool
+      dua # TUI disk space tool
+      fastfetch # Neofetch but faster
+      fd # Sexy find
+      fzf # Fuzzy search tool
+      gh # GitHub CLI
+      gitui # TUI for git
+      glow # CLI markdown renderer
+      kubectl # Official kubernetes CLI
+      k9s # Kubernetes cluster TUI
+      lazygit # Another TUI for git
+      ripgrep # Text search
+      sd # Sexy sed
+      starship # Prompt configuration
+      xh # CLI Http Client
+      xplr # TUI File Explorer
+      zellij # Terminal Multiplexer
+      zoxide # Directory Portal
     ];
   };
 }
