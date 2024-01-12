e64b891daa099a1ed3b3ce58d74eb95eb16495b4
Ryan Brink <dev@ryanbr.ink>
2024-01-12 11:36:40 -0500

    chore: misc packages, some neovim config, and comments

--- dots/nvim/init.lua
+++ dots/nvim/init.lua
@@ -82,10 +82,6 @@ require('lazy').setup({
     -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
-
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
@@ -199,6 +195,18 @@ require('lazy').setup({
   },
 
   {
+    -- Rustacean Vim
+    'mrcjkb/rustaceanvim',
+    version = '^3',
+    ft = { 'rust' },
+  },
+
+  {
+    -- Debug Adapter Protocol
+    "mfussenegger/nvim-dap",
+  },
+
+  {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
@@ -270,17 +278,11 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
-  {
-    'mrcjkb/rustaceanvim',
-    version = "^3",
-    ft = { 'rust' },
-  },
-
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
   -- require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.autoformat',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
@@ -590,61 +592,12 @@ require('which-key').register({
   ['<leader>h'] = { 'Git [H]unk' },
 }, { mode = 'v' })
 
--- mason-lspconfig requires that these setup functions are called in this order
--- before setting up the servers.
-require('mason').setup()
-require('mason-lspconfig').setup()
-
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
-local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-      -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-      -- diagnostics = { disable = { 'missing-fields' } },
-    },
-  },
-}
-
 -- Setup neovim lua configuration
 require('neodev').setup()
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
--- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
-
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end,
-}
 
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
--- home/dev.nix
+++ home/dev.nix
@@ -4,8 +4,15 @@
   ...
 }: {
   home.packages = with pkgs; [
-    cargo
-    gcc9
-    zig
+    cargo           # Rust package manager
+    gcc9            # C compiler
+    graphviz-nox    # GraphViz / Dot
+    helmfile        # Declarative helm release management
+    jdk21           # Java 21 JDK
+    kubernetes-helm # Kubernetes "package manager"
+    lldb_9          # Debugger Protocol
+    lua             # Lua
+    rust-analyzer   # LSP for Rust
+    zig             # Zig stdlib
   ];
 }
