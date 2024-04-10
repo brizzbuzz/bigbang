2a393fc0da9ebe4ad9f2457c03d560544f21d113
Ryan Brink <dev@ryanbr.ink>
2024-04-10 10:08:54 -0400

    feat: move cloudy config primarily to modules

--- hosts/cloudy/configuration.nix
+++ hosts/cloudy/configuration.nix
@@ -1,40 +1,30 @@
 {
   config,
+  inputs,
   pkgs,
+  pkgs-unstable,
+  nixos-modules,
   ...
 }: {
   imports = [
     ./hardware-configuration.nix
-    ./networking.nix
+    inputs.home-manager.nixosModules.home-manager
+    # TODO: Can I simply import all of the modules somehow?
+    nixos-modules.boot
+    nixos-modules.environment
+    nixos-modules.flake-support
+    nixos-modules.fonts
+    nixos-modules.garbage-collection
+    nixos-modules.locale
+    nixos-modules.networking
+    nixos-modules.security
+    nixos-modules.users
   ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
+  # TODO: Should I disable this on all hosts?
   systemd.services.NetworkManager-wait-online.enable = false;
-  time.timeZone = "America/New_York";
 
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  security.sudo.wheelNeedsPassword = false;
-
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
+  # TODO: Move to better place
   users.users.god = {
     isNormalUser = true;
     description = "Literally God (of this domain, no disrepect to the true Big G)";
@@ -44,37 +34,6 @@
   };
 
   nix.settings.trusted-users = ["god"];
-
-  nixpkgs.config.allowUnfree = true;
-
-  environment.systemPackages = with pkgs; [
-    git
-    jq
-    neovim
-    nushell
-    wget
-  ];
-
-  environment.variables = {
-    EDITOR = "nvim";
-  };
-
-  # Garbage Collection
-  nix = {
-    # optimise.automatic = true;  TODO: See https://github.com/NixOS/nix/issues/6033
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 14d";
-    };
-  };
-
-  nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
 
   system.stateVersion = "23.11";
 }
