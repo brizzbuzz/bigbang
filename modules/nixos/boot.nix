0fb4a7dbfdac16b830e7be2b58fe51d3e08e2648
Ryan Brink <dev@ryanbr.ink>
2024-04-25 15:47:56 -0400

    chore: bump file watch count

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -3,7 +3,7 @@
     kernelParams = ["psmouse.synaptics_intertouch=0"];
     kernel.sysctl = {
       "kernel.perf_event_paranoid" = 1;
-      "fs.inotify.max_user_watches" = 65536;
+      "fs.inotify.max_user_watches" = 999999;
       "fs.inotify.max_user_instances" = 1024;
     };
 
