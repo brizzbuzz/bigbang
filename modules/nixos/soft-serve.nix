bee8e3ebe77c8abdba3619ff0d4419a58ec85e18
Ryan Brink <5607577+brizzbuzz@users.noreply.github.com>
2024-05-23 09:33:58 -0500

    feat: soft serve (#37)

---
+++ modules/nixos/soft-serve.nix
@@ -0,0 +1,22 @@
+{
+  pkgs-unstable,
+  config,
+  lib,
+  ...
+}: {
+  options = {
+    soft-serve.enable = lib.mkEnableOption "Enable Soft Serve";
+  };
+  config = lib.mkIf config.soft-serve.enable {
+    systemd.user.services.soft-serve = {
+      description = "Soft Serve";
+      wantedBy = ["default.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs-unstable.soft-serve}/bin/soft-serve";
+        Restart = "always";
+        RestartSec = "10";
+      };
+    };
+  };
+}
