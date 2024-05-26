bf1d4e7087dd878c57f5303216b1001f12def119
Ryan Brink <dev@ryanbr.ink>
2024-05-26 15:38:51 -0400

    chore: explicitly set pg version to 16

--- modules/nixos/postgres.nix
+++ modules/nixos/postgres.nix
@@ -1,6 +1,11 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   services.postgresql = {
     enable = true;
+    package = pkgs.postgresql_16;
     authentication = lib.mkOverride 10 ''
       #type database  DBuser  auth-method
       local all       all     trust
