9bae7ffad149efbe096ed8081058d73969883872
Ryan Brink <dev@ryanbr.ink>
2024-06-10 12:11:12 -0400

    fix: switch back to nm from connman cuz connman is pita

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,14 +1,4 @@
 {config, ...}: {
   networking.hostName = config.host.name;
-  services.connman.enable = true;
-  services.connman.wifi.backend = "iwd";
-
-  networking.wireless.iwd = {
-    enable = true;
-    settings = {
-      Settings = {
-        AutoConnect = true;
-      };
-    };
-  };
+  networking.networkmanager.enable = true;
 }
