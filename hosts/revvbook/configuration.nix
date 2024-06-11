ef215d9df78475977b25873b2d01957ddfc36a69
Ryan Brink <ryan@Ryan-Revvbook-Pro.local>
2024-06-11 12:15:19 -0400

    feat: add additional packages to revv laptop

--- hosts/revvbook/configuration.nix
+++ hosts/revvbook/configuration.nix
@@ -3,7 +3,10 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages =
-    [ pkgs.neovim
+    with pkgs; [
+    neovim
+    direnv
+    gitui
     ];
 
    programs.zsh.enable = true;
