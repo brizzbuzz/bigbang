75c79697200c483dab70ff77a832e91a34c6a1a0
Ryan Brink <dev@ryanbr.ink>
2024-10-08 16:39:39 -0400

    feat(nvim): enable telescope

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -6,14 +6,11 @@
   programs.nixvim = {
     enable = true;
     package = pkgs-unstable.neovim-unwrapped;
-
     globals.mapleader = " ";
-
     colorschemes.catppuccin = {
       enable = true;
       settings.flavour = "macchiato";
     };
-
     opts = {
       number = true;
       relativenumber = true;
@@ -28,5 +25,28 @@
       termguicolors = true;
       clipboard = "unnamedplus";
     };
+
+    plugins.telescope = {
+      enable = true;
+      keymaps = {
+        "<leader>sf" = "find_files";
+        "<leader>sg" = "live_grep";
+        "<leader>sb" = "buffers";
+        "<leader>sh" = "help_tags";
+      };
+      extensions = {
+        fzf-native = {
+          enable = true;
+          settings = {
+            fuzzy = true;
+            override_generic_sorter = true;
+            override_file_sorter = true;
+            case_mode = "smart_case";
+          };
+        };
+      };
+    };
+
+    plugins.web-devicons.enable = true;
   };
 }
