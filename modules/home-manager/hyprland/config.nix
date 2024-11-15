6ce07744ec6909fcdfcb09ecea30f69f48f727d3
Ryan Brink <dev@ryanbr.ink>
2024-11-15 15:25:25 -0500

    fix: comment out shadow hypr config section, broken on latest version

--- modules/home-manager/hyprland/config.nix
+++ modules/home-manager/hyprland/config.nix
@@ -70,8 +70,10 @@ in {
         passes = 1;
       };
       drop_shadow = "yes";
-      shadow_range = 4;
-      shadow_render_power = 3;
+      # shadow = {
+      #   range = 4;
+      #   render_power = 3;
+      # };
       "col.shadow" = "rgba(1a1a1aee)";
     };
 
