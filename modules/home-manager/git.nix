ae21cb5933da683838733eda0a5b1e193f60ff3f
Ryan Brink <dev@ryanbr.ink>
2025-01-19 11:26:50 -0500

    blah

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -10,79 +10,61 @@
     email = "ryan@withodyssey.com";
     signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAvZU9QjyJpanD7LGnSn4e5gcOdLqL8nkUYfowWyrFvl";
   };
+
+  opPath = if isDarwin then "/Applications/1Password.app/Contents/MacOS/op-ssh-sign" else "/run/current-system/sw/bin/op-ssh-sign";
+  opSockPath = if isDarwin then "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" else "~/.1password/agent.sock";
 in {
   programs.git = {
     enable = true;
     userName = "Ryan Brink";
     userEmail = "dev@ryanbr.ink";
-
-    includes = [
-    {
-      condition = "gitdir:~/Workspace/withodyssey/**";  # Note the ** for recursive matching
-      contents = {
-        user = workUser;
-      };
-    }
-    {
-      condition = "gitdir:~/Workspace/ryan-odyssey/**";
-      contents = {
-        user = workUser;
-      };
-    }
-  ];
-
-    difftastic = {
-      enable = true;
-      background = "dark";
+    signing = {
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP+4LZpJ9+QmvjLKMzmHX1aUdsnoOlrrcTjwKhcwnCN1";
+      signByDefault = true;
     };
 
+    includes = [
+      {
+        condition = "gitdir:~/Workspace/withodyssey/**";
+        contents = {
+          user = workUser;
+          core.sshCommand = "ssh -i '${opSockPath}' -o IdentityAgent='${opSockPath}'";
+        };
+      }
+      {
+        condition = "gitdir:~/Workspace/ryan-odyssey/**";
+        contents = {
+          user = workUser;
+          core.sshCommand = "ssh -i '${opSockPath}' -o IdentityAgent='${opSockPath}'";
+        };
+      }
+    ];
+
     aliases = {
-      blah = "git add .; git commit -m 'blah'; git push";
+      blah = "!git add .; git commit -m 'blah'; git push";
       co = "checkout";
       cs = "commit --allow-empty-message -m ''";
       cm = "commit -m";
       dft = "difftool";
       dlog = "!f() { GIT_EXTERNAL_DIFF=difft git log -p --ext-diff $@; }; f";
-      main = "git co main; git pl";
+      main = "!git checkout main; git pull";
       pl = "pull";
       ps = "push";
     };
 
     extraConfig = {
-      commit = {
-        gpgsign = true;
-      };
-      diff = {
-        tool = "difftastic";
-      };
-      difftool = {
-        prompt = false;
-      };
-      "difftool \"difftastic\"" = {
-        cmd = "difft \"$LOCAL\" \"$REMOTE\"";
-      };
-      init = {
-        defaultBranch = "main";
-      };
-      gpg = {
-        format = "ssh";
-      };
+      commit.gpgsign = true;
+      gpg.format = "ssh";
       "gpg \"ssh\"" = lib.mkIf isDesktop {
-        program =
-          if isDarwin
-          then "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
-          else "/run/current-system/sw/bin/op-ssh-sign";
+        program = opPath;
       };
-      push = {
-        autoSetupRemote = true;
-      };
-      user = {
-        signingKey = osConfig.host.gitSigningKey;
-      };
+      push.autoSetupRemote = true;
+      core.sshCommand = "ssh -o IdentityAgent='${opSockPath}'";
     };
-  };
 
-  programs.git-cliff = {
-    enable = true;
+    difftastic = {
+      enable = true;
+      background = "dark";
+    };
   };
 }
--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -1,59 +1,20 @@
 {
   lib,
-  pkgs,
   osConfig,
   ...
 }: let
-  isDarwin = pkgs.stdenv.isDarwin;
   isDesktop = osConfig.host.desktop.enable;
-
-  # 1Password agent config
-  opAgentConfig = ''
-    # First, specify work keys from Odyssey vault
-    [[ssh-keys]]
-    vault = "Odyssey"
-    account = "my.1password.com"
-
-    # Then add personal keys
-    [[ssh-keys]]
-    vault = "Private"
-    account = "my.1password.com"
-  '';
-
-  # Define the agent socket path
-  opAgentSocket = if isDarwin
-    then "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-    else "~/.1password/agent.sock";
 in {
   programs.ssh = lib.mkIf isDesktop {
     enable = true;
 
-    extraConfig = ''
-      IdentityAgent "${opAgentSocket}"
-    '';
-
-    matchBlocks = {
-      # Personal GitHub (default)
-      "github.com" = {
-        hostname = "github.com";
-        user = "git";
-        identitiesOnly = true;
-      };
-
-      # Work GitHub configuration
-      "github.com-work" = {
-        hostname = "github.com";
-        user = "git";
-        identitiesOnly = true;
-      };
-
-      # Your existing server config
-      "cloudy" = {
-        hostname = "cloudy";
-        forwardAgent = true;
-        user = "ryan";
-      };
-    };
+    # matchBlocks = {
+    #   "cloudy" = {
+    #     hostname = "cloudy";
+    #     forwardAgent = true;
+    #     user = "ryan";
+    #   };
+    # };
 
     forwardAgent = false;
     addKeysToAgent = "no";
@@ -62,16 +23,5 @@ in {
     serverAliveCountMax = 3;
     hashKnownHosts = false;
     userKnownHostsFile = "~/.ssh/known_hosts";
-    controlMaster = "no";
-    controlPath = "~/.ssh/master-%r@%n:%p";
-    controlPersist = "no";
-  };
-
-  # Create 1Password SSH agent config file
-  home.file.".config/1Password/ssh/agent.toml".text = opAgentConfig;
-
-  # Set the environment variable
-  home.sessionVariables = {
-    SSH_AUTH_SOCK = opAgentSocket;
   };
 }
