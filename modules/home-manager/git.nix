761de706494eea221038d7bc6adce4e5fc425d66
Ryan Brink <dev@ryanbr.ink>
2025-01-19 11:33:43 -0500

    blah

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -6,65 +6,64 @@
 }: let
   isDarwin = pkgs.stdenv.isDarwin;
   isDesktop = osConfig.host.desktop.enable;
-  workUser = {
-    email = "ryan@withodyssey.com";
-    signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAvZU9QjyJpanD7LGnSn4e5gcOdLqL8nkUYfowWyrFvl";
-  };
-
-  opPath = if isDarwin then "/Applications/1Password.app/Contents/MacOS/op-ssh-sign" else "/run/current-system/sw/bin/op-ssh-sign";
-  opSockPath = if isDarwin then "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" else "~/.1password/agent.sock";
 in {
   programs.git = {
     enable = true;
     userName = "Ryan Brink";
     userEmail = "dev@ryanbr.ink";
-    signing = {
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP+4LZpJ9+QmvjLKMzmHX1aUdsnoOlrrcTjwKhcwnCN1";
-      signByDefault = true;
-    };
-
-    includes = [
-      {
-        condition = "gitdir:~/Workspace/withodyssey/**";
-        contents = {
-          user = workUser;
-          core.sshCommand = "ssh -o IdentityAgent=\"${opSockPath}\"";
-        };
-      }
-      {
-        condition = "gitdir:~/Workspace/ryan-odyssey/**";
-        contents = {
-          user = workUser;
-          core.sshCommand = "ssh -o IdentityAgent=\"${opSockPath}\"";
-        };
-      }
-    ];
-
-    aliases = {
-      blah = "!git add .; git commit -m 'blah'; git push";
-      co = "checkout";
-      cs = "commit --allow-empty-message -m ''";
-      cm = "commit -m";
-      dft = "difftool";
-      dlog = "!f() { GIT_EXTERNAL_DIFF=difft git log -p --ext-diff $@; }; f";
-      main = "!git checkout main; git pull";
-      pl = "pull";
-      ps = "push";
-    };
-
-    extraConfig = {
-      commit.gpgsign = true;
-      gpg.format = "ssh";
-      "gpg \"ssh\"" = lib.mkIf isDesktop {
-        program = opPath;
-      };
-      push.autoSetupRemote = true;
-      core.sshCommand = "ssh -o IdentityAgent=\"${opSockPath}\"";
-    };
 
     difftastic = {
       enable = true;
       background = "dark";
     };
+
+    aliases = {
+      blah = "git add .; git commit -m 'blah'; git push";
+      co = "checkout";
+      cs = "commit --allow-empty-message -m ''";
+      cm = "commit -m";
+      dft = "difftool";
+      dlog = "!f() { GIT_EXTERNAL_DIFF=difft git log -p --ext-diff $@; }; f";
+      main = "git co main; git pl";
+      pl = "pull";
+      ps = "push";
+    };
+
+    extraConfig = {
+      commit = {
+        gpgsign = true;
+      };
+      diff = {
+        tool = "difftastic";
+      };
+      difftool = {
+        prompt = false;
+      };
+      "difftool \"difftastic\"" = {
+        cmd = "difft \"$LOCAL\" \"$REMOTE\"";
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      gpg = {
+        format = "ssh";
+      };
+      "gpg \"ssh\"" = lib.mkIf isDesktop {
+        program =
+          if isDarwin
+          then "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
+          else "/run/current-system/sw/bin/op-ssh-sign";
+      };
+      push = {
+        autoSetupRemote = true;
+      };
+      user = {
+        signingKey = osConfig.host.gitSigningKey;
+      };
+    };
+  };
+
+  programs.git-cliff = {
+    enable = true;
   };
 }
--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -1,20 +1,31 @@
 {
   lib,
+  pkgs,
   osConfig,
   ...
 }: let
+  isDarwin = pkgs.stdenv.isDarwin;
   isDesktop = osConfig.host.desktop.enable;
 in {
   programs.ssh = lib.mkIf isDesktop {
     enable = true;
 
-    # matchBlocks = {
-    #   "cloudy" = {
-    #     hostname = "cloudy";
-    #     forwardAgent = true;
-    #     user = "ryan";
-    #   };
-    # };
+    extraConfig =
+      if isDarwin
+      then ''
+        IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+      ''
+      else ''
+        IdentityAgent "~/.1password/agent.sock"
+      '';
+
+    matchBlocks = {
+      "cloudy" = {
+        hostname = "cloudy";
+        forwardAgent = true;
+        user = "ryan";
+      };
+    };
 
     forwardAgent = false;
     addKeysToAgent = "no";
@@ -23,5 +34,8 @@ in {
     serverAliveCountMax = 3;
     hashKnownHosts = false;
     userKnownHostsFile = "~/.ssh/known_hosts";
+    controlMaster = "no";
+    controlPath = "~/.ssh/master-%r@%n:%p";
+    controlPersist = "no";
   };
 }
