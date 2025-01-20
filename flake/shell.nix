82e87e9b0d29fec0fb1acda23bc6605ed22b8cf9
Ryan Brink <dev@ryanbr.ink>
2025-01-20 00:52:58 -0500

    blah

--- flake/shell.nix
+++ flake/shell.nix
@@ -6,7 +6,6 @@ forAllSystems (system: {
   default = pkgs.${system}.mkShell {
     packages = with pkgs.${system}; [
       git-cliff # Changelog generator
-      nixos-generators # ISO and other image format generation
       nurl # Nix Fetcher Generator
       tokei # Code statistics
     ];
