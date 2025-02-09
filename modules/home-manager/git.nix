05ed077de756dd65d071eb045a3bd1d15dd895cd
Ryan Brink <5607577+brizzbuzz@users.noreply.github.com>
2025-02-09 16:12:10 -0500

    feat(git): streamline git configuration and user settings (#52)

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -30,40 +30,36 @@ in {
     };
 
     extraConfig = {
-      commit = {
-        gpgsign = true;
-      };
-      diff = {
-        tool = "difftastic";
-      };
-      difftool = {
-        prompt = false;
-      };
+      commit.gpgsign = true;
+      diff.tool = "difftastic";
+      difftool.prompt = false;
       "difftool \"difftastic\"" = {
         cmd = "difft \"$LOCAL\" \"$REMOTE\"";
       };
-      init = {
-        defaultBranch = "main";
-      };
-      gpg = {
-        format = "ssh";
-      };
+      init.defaultBranch = "main";
+      gpg.format = "ssh";
       "gpg \"ssh\"" = lib.mkIf isDesktop {
         program =
           if isDarwin
           then "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
           else "/run/current-system/sw/bin/op-ssh-sign";
       };
-      push = {
-        autoSetupRemote = true;
-      };
-      user = {
-        signingKey = osConfig.host.gitSigningKey;
-      };
+      push.autoSetupRemote = true;
+      user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP+4LZpJ9+QmvjLKMzmHX1aUdsnoOlrrcTjwKhcwnCN1";
     };
+
+    includes = [
+      {
+        condition = "gitdir:**/withodyssey/**";
+        contents = {
+          user = {
+            email = "ryan@withodyssey.com";
+            signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAvZU9QjyJpanD7LGnSn4e5gcOdLqL8nkUYfowWyrFvl";
+          };
+        };
+      }
+    ];
   };
 
-  programs.git-cliff = {
-    enable = true;
-  };
+  programs.git-cliff.enable = true;
 }
