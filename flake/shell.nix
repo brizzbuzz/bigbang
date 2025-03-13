37e4806f0cb9a53ed4e97074f6aabd92d2a72b01
Ryan Brink <5607577+brizzbuzz@users.noreply.github.com>
2025-03-13 19:27:23 -0400

    chore: qol updates (#59)

--- flake/shell.nix
+++ flake/shell.nix
@@ -6,8 +6,10 @@
 forAllSystems (system: {
   default = pkgs-master.${system}.mkShell {
     packages = with pkgs-master.${system}; [
+      alejandra # Nix Formatter
       claude-code # Claude Code
       git-cliff # Changelog generator
+      nil # Nix LSP
       nurl # Nix Fetcher Generator
       tokei # Code statistics
     ];
