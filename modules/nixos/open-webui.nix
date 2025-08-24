24fdd639071b500bbc2f416ff1af7feef80f33b1
Ryan Brink <dev@ryanbr.ink>
2025-08-24 18:21:07 -0400

    fix: openwebui setup

--- modules/nixos/open-webui.nix
+++ modules/nixos/open-webui.nix
@@ -5,10 +5,15 @@
 }: {
   services.open-webui = lib.mkIf config.host.ai.enable {
     enable = true;
+    host = "0.0.0.0";
     port = config.ports.open-webui;
     environment = {
       OLLAMA_API_BASE_URL = "http://localhost:${toString config.ports.ollama.api}";
-      WEBUI_AUTH = "False";
+      WEBUI_AUTH = "True";
+      DATABASE_URL = "postgresql://openwebui:openwebui@localhost:5432/openwebui";
+      VECTOR_DB = "pgvector";
+      ENABLE_SIGNUP = "True";
+      DEFAULT_USER_ROLE = "pending";
     };
   };
 
