17d6bec4c65e7e31adf7b5a521151b918a0191f8
Ryan Brink <dev@ryanbr.ink>
2025-09-27 11:33:39 -0400

    chore: add notion mcp to zed and quiet direnv logging

--- modules/home-manager/direnv.nix
+++ modules/home-manager/direnv.nix
@@ -3,5 +3,17 @@
     enable = true;
     enableNushellIntegration = true;
     nix-direnv.enable = true;
+
+    # Make direnv quiet - no loading/unloading messages
+    config = {
+      global = {
+        hide_env_diff = true;
+      };
+    };
+  };
+
+  # Set DIRENV_LOG_FORMAT to empty string to silence direnv messages
+  home.sessionVariables = {
+    DIRENV_LOG_FORMAT = "";
   };
 }
