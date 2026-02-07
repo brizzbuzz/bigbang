bbd8f96ecd12b91824a9bdddec800815caee131a
Ryan Brink <dev@ryanbr.ink>
2026-02-06 20:00:59 -0500

    feat(frame): disko config

--- hosts/frame/configuration.nix
+++ hosts/frame/configuration.nix
@@ -1,6 +1,8 @@
-{...}: {
+{inputs, ...}: {
   imports = [
+    inputs.disko.nixosModules.disko
     ./hardware-configuration.nix
+    ./disko.nix
     ../../modules/common
     ../../modules/nixos
   ];
