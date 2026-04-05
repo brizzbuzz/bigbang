e7be52d5397e21b06a88635cd4e2bf7ea3569053
Ryan Brink <5607577+brizzbuzz@users.noreply.github.com>
2026-04-05 14:03:00 -0400

    fix: deploy nushell autoload scripts (#206)

--- modules/common/users/config-files/configs/nushell.nix
+++ modules/common/users/config-files/configs/nushell.nix
@@ -6,6 +6,8 @@
   isDarwin = pkgs.stdenv.isDarwin;
   nushellConfig = builtins.readFile ../files/nushell/config.nu;
   nushellEnv = builtins.readFile ../files/nushell/env.nu;
+  nushellAutoloadDir = ../files/nushell/autoload;
+  nushellAutoloadFiles = lib.filterAttrs (name: type: type == "regular" && lib.hasSuffix ".nu" name) (builtins.readDir nushellAutoloadDir);
 in {
   mkNushellScript = {
     userName,
@@ -21,7 +23,9 @@ in {
   in
     lib.optionalString enabled ''
         # Nushell configuration
+        [ -L "${nushellConfigDir}/autoload" ] && rm "${nushellConfigDir}/autoload"
         mkdir -p "${nushellConfigDir}"
+        mkdir -p "${nushellConfigDir}/autoload"
 
         # Remove old symlinks if they exist (from home-manager)
         [ -L "${nushellConfigDir}/config.nu" ] && rm "${nushellConfigDir}/config.nu"
@@ -36,6 +40,18 @@ in {
       ${nushellEnv}
       EOFNUENV
         chmod 644 "${nushellConfigDir}/env.nu"
+
+      ${lib.concatMapStringsSep "\n" (
+        fileName: let
+          fileContents = builtins.readFile (nushellAutoloadDir + "/${fileName}");
+        in ''
+            [ -L "${nushellConfigDir}/autoload/${fileName}" ] && rm "${nushellConfigDir}/autoload/${fileName}"
+            cat > "${nushellConfigDir}/autoload/${fileName}" << 'EOFNUAUTOLOAD_${lib.toUpper (lib.replaceStrings ["."] ["_"] fileName)}'
+          ${fileContents}
+          EOFNUAUTOLOAD_${lib.toUpper (lib.replaceStrings ["."] ["_"] fileName)}
+            chmod 644 "${nushellConfigDir}/autoload/${fileName}"
+        ''
+      ) (builtins.attrNames nushellAutoloadFiles)}
 
         ${lib.optionalString isDarwin ''
         chown -R "${userName}:${userGroup}" "${nushellConfigDir}" 2>/dev/null || true
