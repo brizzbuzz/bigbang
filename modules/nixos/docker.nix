b8e40221df135fbba3a890361f2efaae9bf7fc75
Ryan Brink <dev@ryanbr.ink>
2026-04-07 10:26:19 -0400

    fix(docker): align firewall backend with nftables

--- modules/nixos/docker.nix
+++ modules/nixos/docker.nix
@@ -1,3 +1,16 @@
-{
-  virtualisation.docker.enable = true;
+{pkgs, ...}: {
+  virtualisation.docker = {
+    enable = true;
+    daemon.settings = {
+      "firewall-backend" = "nftables";
+    };
+  };
+
+  # Docker 29's nftables backend shells out to `nft`.
+  systemd.services.docker.path = [pkgs.nftables];
+
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+  };
 }
