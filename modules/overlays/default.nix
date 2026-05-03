c28f8fe67ef8e721ed2332e590fcc3c0e3702635
Ryan Brink <dev@ryanbr.ink>
2026-05-03 10:44:38 -0400

    feat(netbird): package combined server

--- modules/overlays/default.nix
+++ modules/overlays/default.nix
@@ -1,4 +1,8 @@
 [
+  (_final: prev: {
+    netbird-server = prev.callPackage ./packages/netbird-server.nix {};
+  })
+
   # Disable checks for fish shell
   (_final: prev: {
     fish = prev.fish.overrideAttrs (_oldAttrs: {
