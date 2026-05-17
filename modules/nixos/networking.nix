3a8934ae32a1b5cca1f86ee95fb5a01b3f10bfbc
Ryan Brink <dev@ryanbr.ink>
2026-05-17 11:30:32 -0400

    fix: split homelab DNS from public resolution

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,13 +1,3 @@
-{
-  config,
-  lib,
-  ...
-}: {
-  networking = {
-    hostName = config.host.name;
-
-    # Use the UniFi router as the DNS server by default
-    # This ensures proper resolution of local lan.rgbr.ink domains
-    nameservers = lib.mkDefault ["192.168.11.1"];
-  };
+{config, ...}: {
+  networking.hostName = config.host.name;
 }
